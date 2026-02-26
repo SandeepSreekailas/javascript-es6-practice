1d80577536fa097d6b87306fd6254f8bc9e763f8
SandeepSreekailas <sandeepsreekailas123@gmail.com.com>
2026-02-26 10:09:28 +0530

    add ES6 module import and export examples

---
+++ ES6modules.js
@@ -0,0 +1,8 @@
+// ES6-Modules :- A module is a file containing a set of functions, variables, or classes. 
+//                Modules are imported into other files to reuse their code.
+
+import { pi, area,circumference}  from "./Mathutils.js" ;
+
+console.log(pi);
+console.log(area(6));
+console.log(circumference(5));
---
+++ Mathutils.js
@@ -0,0 +1,9 @@
+export let pi=3.141
+
+export function area(radius){
+    return pi*radius*radius
+}
+
+export function circumference(radius){
+    return 2*pi*radius
+}
